0a5912691b5f51fde176a34377a538fabbe8ff8d
smboolean63 <smboolean63@madrigali.dev>
2022-06-07 11:42:04 +0200

    aggiunto header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <BaseHeader/>
   </div>
 </template>
 
 <script>
+import BaseHeader from './components/blocks/BaseHeader.vue';
+
 export default {
   name: 'App',
   components: {
+    BaseHeader,
   }
 }
 </script>
---
+++ src/components/blocks/BaseHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+      <div class="container">
+        <h1>Rick and Morty app</h1>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'BaseHeader',
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    padding: 1.875rem 0;
+}
+</style>
